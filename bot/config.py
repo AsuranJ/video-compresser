99f55a514a313aa347d1d4f3d697058547fc6f3f
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-10-13 02:14:40 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,6 @@ class Config(object):
     API_HASH = get_config("API_HASH")
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/DOWNLOADS")
     # Telegram maximum file upload size
